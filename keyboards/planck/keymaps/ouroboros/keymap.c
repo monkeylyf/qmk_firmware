8561479c214d8bdc897091578434ef9e94c7c937
Yifeng Liu <laituan1986@gmail.com>
2016-08-16 17:13:12 -0700

    Updated my Planck keymap

--- keyboards/planck/keymaps/ouroboros/keymap.c
+++ keyboards/planck/keymaps/ouroboros/keymap.c
@@ -37,32 +37,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FUNC | Ctrl | Alt  | GUI  | Symb | Bksp |Enter |Space |   (  |   )  |  [   |  ]   |
+ * | FUNC | Ctrl | Alt  | GUI  | Symb | Bksp |Enter |Space |   [  |   ]  |  \   |  `   |
  * `-----------------------------------------------------------------------------------'
+ *
  */
 [_BASE] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL },
-  {FUNC,    KC_LCTL, KC_LALT, KC_LGUI, SYMB,  KC_BSPC, KC_ENT,  KC_SPC,  KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC}
+  {FUNC,    KC_LCTL, KC_LALT, KC_LGUI, SYMB,  KC_BSPC, KC_ENT,  KC_SPC,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV }
 },
 
 /* SYMB
  * ,-----------------------------------------------------------------------------------.
- * |  Esc |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Esc  |
+ * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |-----------------------------------------------------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   ~  |   |  |   +  |   [  |   ]  |  {   |   }  |   =  |   \  |   `  |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Bksp |Enter |Space |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_SYMB] = {
-  {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC },
-  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL },
-  {_______, KC_TILD, KC_PIPE, KC_PLUS, KC_LBRC, KC_RBRC, KC_RCBR, KC_RBRC, KC_EQL,  KC_BSLS, KC_GRV,  _______},
-  {_______, _______, _______, _______, _______, KC_BSPC, KC_ENT,  KC_SPC,  _______, _______, _______, _______}
+  {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ },
+  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______ },
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* MOVE
@@ -114,7 +115,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_MOVE, _SYMB, _FUNC);
       }
       return false;
-      break;
     case SYMB:
       if (record->event.pressed) {
         layer_on(_SYMB);
@@ -124,7 +124,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_MOVE, _SYMB, _FUNC);
       }
       return false;
-      break;
     case FUNC:
       if (record->event.pressed) {
         layer_on(_FUNC);
@@ -132,7 +131,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_FUNC);
       }
       return false;
-      break;
   }
   return true;
 }
