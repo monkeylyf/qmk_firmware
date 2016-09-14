bcd58e60b1d58063d6d543d8f38114f77aeecf4a
Yifeng Liu <laituan1986@gmail.com>
2016-09-14 14:09:13 -0700

    ouroboros-planck: updated layout inspired by jeebak's one

--- keyboards/planck/keymaps/ouroboros/keymap.c
+++ keyboards/planck/keymaps/ouroboros/keymap.c
@@ -11,76 +11,91 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _MOVE 1
-#define _SYMB 2
+#define _QWERTY 0
+#define _NUMBER 1
+#define _SYMBOL 2
 #define _FUNC 3
 
 enum planck_keycodes {
-  BASE = SAFE_RANGE,
-  MOVE,
-  SYMB,
+  QWERTY = SAFE_RANGE,
+  NUMBER,
+  SYMBOL,
   FUNC
+};
+
+enum macro_keycodes {
+  KC_CMD_TAB,
+  KC_AG_FIND,
+  KC_AG_AGAIN,
+  KC_AG_UNDO,
+  KC_AG_CUT,
+  KC_AG_COPY,
+  KC_AG_PASTE,
+  KC_LSFT_MINS,
+  KC_LSFT_EQL,
 };
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+// Macro definitions.
+#define LSFT_MINS   M(KC_LSFT_MINS)     // Underscore sign.
+#define LSFT_EQL    M(KC_LSFT_EQL)      // Plus sign.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* BASE
+/* QWERTY
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  `   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  =   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FUNC | Ctrl | Alt  | GUI  | Symb | Bksp |Enter |Space |   [  |   ]  |  \   |  `   |
+ * | FUNC | Ctrl | Alt  | GUI  | Symb | Bksp |Enter |Space |Number|   [  |   ]  |      |
  * `-----------------------------------------------------------------------------------'
  *
  */
-[_BASE] = {
-  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL },
-  {FUNC,    KC_LCTL, KC_LALT, KC_LGUI, SYMB,  KC_BSPC, KC_ENT,  KC_SPC,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV }
+[_QWERTY] = {
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV },
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS},
+  {FUNC,    KC_LCTL, KC_LALT, KC_LGUI, SYMBOL, KC_BSPC, KC_ENT,  KC_SPC,  NUMBER,  KC_LBRC, KC_RBRC, _______}
 },
 
-/* SYMB
+/* SYMBOL
  * ,-----------------------------------------------------------------------------------.
- * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
- * |-----------------------------------------------------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
+ * |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   _  |  +   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      | Left | Down |  Up  | Right|      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_SYMB] = {
-  {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ },
-  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______ },
+[_SYMBOL] = {
+  {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  LSFT_MINS, LSFT_EQL},
+  {_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,   _______ },
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,   _______ },
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,   _______ }
+},
+
+/* NUMBER
+ * ,-----------------------------------------------------------------------------------.
+ * |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |  =   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NUMBER] = {
+  {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
-
-/* MOVE
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |      | Home |  Up  | End  |      |      | Home |  Up  | End  |      | Esc  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      | Left | Down |Right |      |      | Left | Down |Right |      | Del  |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |Pg Up |Pg Dn |      |      |Pg Dn |Pg Up |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_MOVE] = {
-  {KC_ESC,  _______, KC_HOME, KC_UP,   KC_END,  _______, _______, KC_HOME, KC_UP,   KC_END,  _______, KC_ESC },
-  {KC_DEL,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_DEL },
-  {_______, _______, _______, KC_PGUP, KC_PGDN, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
@@ -104,24 +119,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+// Audio setup.
+#ifdef AUDIO_ENABLE
+float tone_startup[][2] = {
+    ED_NOTE(_E7 ),
+    E__NOTE(_CS7),
+    E__NOTE(_E6 ),
+    E__NOTE(_A6 ),
+    M__NOTE(_CS7, 20)
+};
+
+float tone_qwerty[][2]    = SONG(QWERTY_SOUND);
+float tone_plover_gb[][2] = SONG(PLOVER_GOODBYE_SOUND);
+float tone_goodbye[][2]   = SONG(GOODBYE_SOUND);
+float music_scale[][2]    = SONG(MUSIC_SCALE_SOUND);
+#endif
+
+void matrix_init_user(void) {
+  #ifdef AUDIO_ENABLE
+    startup_user();
+  #endif
+}
+
+#ifdef AUDIO_ENABLE
+void startup_user() {
+  _delay_ms(20);  // gets rid of tick.
+  //PLAY_NOTE_ARRAY(tone_startup, false, 0);
+  PLAY_NOTE_ARRAY(music_scale, false, 0);
+}
+
+void shutdown_user() {
+  PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
+  _delay_ms(150);
+  stop_all_notes();
+}
+
+void music_on_user(void) {
+  music_scale_user();
+}
+
+void music_scale_user(void) {
+  PLAY_NOTE_ARRAY(music_scale, false, 0);
+}
+#endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case MOVE:
+    case QWERTY:
       if (record->event.pressed) {
-        layer_on(_MOVE);
-        update_tri_layer(_MOVE, _SYMB, _FUNC);
-      } else {
-        layer_off(_MOVE);
-        update_tri_layer(_MOVE, _SYMB, _FUNC);
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+        #endif
+        default_layer_set(1UL << _QWERTY);
       }
       return false;
-    case SYMB:
+      break;
+    case NUMBER:
       if (record->event.pressed) {
-        layer_on(_SYMB);
-        update_tri_layer(_MOVE, _SYMB, _FUNC);
+        layer_on(_NUMBER);
       } else {
-        layer_off(_SYMB);
-        update_tri_layer(_MOVE, _SYMB, _FUNC);
+        layer_off(_NUMBER);
+      }
+      return false;
+    case SYMBOL:
+      if (record->event.pressed) {
+        layer_on(_SYMBOL);
+      } else {
+        layer_off(_SYMBOL);
       }
       return false;
     case FUNC:
@@ -133,4 +197,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
   }
   return true;
+}
+
+// Macro events.
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  switch (id) {
+    case KC_LSFT_EQL:
+      return MACRODOWN(D(LSFT), T(EQL), END);
+      break;
+    case KC_LSFT_MINS:
+      return MACRODOWN(D(LSFT), T(MINS), END);
+      break;
+  }
+  return MACRO_NONE;
 }
