43c8c5b013d78b16f7cf93b607fd7ca0dbda5068
Yifeng Liu <laituan1986@gmail.com>
2016-11-09 17:14:59 -0800

    ouroboros-ergo-infinity: save. moving workspace to another mac

--- keyboards/ergodox/keymaps/ouroboros/keymap.c
+++ keyboards/ergodox/keymaps/ouroboros/keymap.c
@@ -17,16 +17,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |  Alt |  CMD | Space|                                       | Space| Down |   [  |   ]  | ~L1  |
- *   `----------------------------------'                                       `----------------------------------'
+ *   |Grv/L1|  Ctl |  Alt |  CMD | Space|                                       | Space| Left | Down |  Up  | Right  |
+ *   `----------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |Backsp|Backsp|------|       |------|  Tab   |Enter |
+ *                                 |ace   |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -35,18 +35,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,          KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
         KC_TAB,          KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),
-        KC_BSPC,         KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
+        KC_ESC,          KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,         KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
-        LT(SYMB,KC_GRV), KC_LALT,  KC_LSFT, KC_LGUI, KC_SPC,
+        LT(SYMB,KC_GRV), KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,
                                                      ALT_T(KC_APP),    KC_LGUI,
                                                                        KC_HOME,
-                                            KC_SPC,  KC_BSPC,          KC_END,
+                                            KC_BSPC, KC_BSPC,          KC_END,
         // right hand
         KC_GRV,          KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         TG(SYMB),        KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                          KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         MEH_T(KC_NO),    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                   KC_SPC,  KC_DOWN, KC_LBRC, KC_RBRC, KC_FN1,
+                                   KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
                              KC_LALT,        CTL_T(KC_ESC),
                              KC_PGUP,
                              KC_PGDN,KC_TAB, KC_ENT
