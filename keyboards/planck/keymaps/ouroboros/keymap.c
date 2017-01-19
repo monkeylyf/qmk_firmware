d4d7b2a031ba18f12cb7b1754e26ebc3add8971b
Yifeng Liu <laituan1986@gmail.com>
2017-01-19 11:38:48 -0800

    ouroboros:planck added right shift and move tilde down by one key position

--- keyboards/planck/keymaps/ouroboros/keymap.c
+++ keyboards/planck/keymaps/ouroboros/keymap.c
@@ -57,17 +57,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Symb |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Sf/Tab|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  `   |
+ * |Sf/Tab|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Func | Ctrl | Alt  | GUI/-| Bksp |  ESC |Enter |Space |   =  |      |      |      |
+ * | Func | Ctrl | Alt  | GUI/-| Bksp |  ESC |Enter |Space |   =  |      |      |  `   |
  * `-----------------------------------------------------------------------------------'
  *
  */
 [_QWERTY] = {
-  {NUMBER,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS},
-  {SYMBOL,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {SFT_TAB, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_GRV },
-  {FUNC,    KC_LCTL, KC_LALT, GUI_MINS, KC_BSPC, KC_ESC, KC_ENT, KC_SPC, KC_EQL,  _______, _______, _______}
+  {NUMBER,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS  },
+  {SYMBOL,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT  },
+  {SFT_TAB, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT},
+  {FUNC,    KC_LCTL, KC_LALT, GUI_MINS, KC_BSPC, KC_ESC, KC_ENT, KC_SPC, KC_EQL,  _______, _______, KC_GRV   }
 },
 
 /* SYMBOL
